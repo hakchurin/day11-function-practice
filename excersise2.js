dabd9d5986150d09435e075f16d3d8bb129aca8f
Haley Akchurin <hdakchurin@gmail.com>
2016-07-31 23:04:37 -0500

    day11 func prac

---
+++ excersise2.js
@@ -0,0 +1,92 @@
+// ---------------------
+// Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
+// ---------------------
+// var number1 = x;
+// var number2 = y;
+//
+// function Math.max(x, y)) {
+//     //Math.max(x,y);
+//
+//     if (x > y) {
+//         return (x);
+//     } else {
+//         (y);
+//     }
+//     console.log(5,6);
+
+
+
+
+    // ---------------------
+    // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
+    // ---------------------
+    // var x = 0;
+    // var y = 1;
+    // var z = 2;
+    //
+    // function maxOfThree(x, y, z) {
+    //     return Math.max(x, y, z);
+    //
+    // }
+    // console.log(maxOfThree(1, 9, 3));
+
+
+
+
+    // ---------------------
+    // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+    // ---------------------
+    //
+    // function isVowel(vowel) {
+    //     var str = 'aeiou';
+    //
+    //
+    //     if (str.includes(vowel)) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
+
+
+    // console.log(isVowel('a'));
+    // return str.split('').reverse().join('');
+
+
+
+
+    // ---------------------
+    // Write a function translate() that will translate a text into "Rovarspraket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
+    // ---------------------
+
+    // function rovarspraket(phrase) {
+    //   var newString = '';
+    //
+    //   phrase.split('').forEach(function(character){
+    //     if (!isVowel(character)){
+    //       newString += character + 'o' + character;
+    //     } else{
+    //       newString += character;
+    //     }
+    //   });
+    //   return newString.replace(' o', ' ');
+    // }
+    // console.log(rovarspraket('this is fun'));
+
+
+
+
+
+
+    // ---------------------
+    // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
+    // ---------------------
+
+
+
+    //     function reverseString(str) {
+    //         return str.split('').reverse().join('');
+    //     }
+    //     reverseString('Haley');
+    //
+    // console.log(reverseString('haley'));
