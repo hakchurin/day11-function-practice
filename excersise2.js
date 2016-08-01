1fdc3d4f8d1637b2f81a95340e62d6a5f9620c04
Haley Akchurin <hdakchurin@gmail.com>
2016-07-31 23:06:12 -0500

    func prac day 11

--- excersise2.js
+++ excersise2.js
@@ -1,18 +1,18 @@
 // ---------------------
 // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
 // ---------------------
-// var number1 = x;
-// var number2 = y;
-//
-// function Math.max(x, y)) {
-//     //Math.max(x,y);
-//
-//     if (x > y) {
-//         return (x);
-//     } else {
-//         (y);
-//     }
-//     console.log(5,6);
+var number1 = x;
+var number2 = y;
+
+function Math.max(x, y)) {
+    //Math.max(x,y);
+
+    if (x > y) {
+        return (x);
+    } else {
+        (y);
+    }
+    console.log(5,6);
 
 
 
@@ -20,15 +20,15 @@
     // ---------------------
     // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
     // ---------------------
-    // var x = 0;
-    // var y = 1;
-    // var z = 2;
-    //
-    // function maxOfThree(x, y, z) {
-    //     return Math.max(x, y, z);
-    //
-    // }
-    // console.log(maxOfThree(1, 9, 3));
+    var x = 0;
+    var y = 1;
+    var z = 2;
+
+    function maxOfThree(x, y, z) {
+        return Math.max(x, y, z);
+
+    }
+    console.log(maxOfThree(1, 9, 3));
 
 
 
@@ -36,21 +36,21 @@
     // ---------------------
     // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
     // ---------------------
-    //
-    // function isVowel(vowel) {
-    //     var str = 'aeiou';
-    //
-    //
-    //     if (str.includes(vowel)) {
-    //         return true;
-    //     } else {
-    //         return false;
-    //     }
-    // }
+
+    function isVowel(vowel) {
+        var str = 'aeiou';
 
 
-    // console.log(isVowel('a'));
-    // return str.split('').reverse().join('');
+        if (str.includes(vowel)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    console.log(isVowel('a'));
+    return str.split('').reverse().join('');
 
 
 
@@ -59,19 +59,19 @@
     // Write a function translate() that will translate a text into "Rovarspraket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
     // ---------------------
 
-    // function rovarspraket(phrase) {
-    //   var newString = '';
-    //
-    //   phrase.split('').forEach(function(character){
-    //     if (!isVowel(character)){
-    //       newString += character + 'o' + character;
-    //     } else{
-    //       newString += character;
-    //     }
-    //   });
-    //   return newString.replace(' o', ' ');
-    // }
-    // console.log(rovarspraket('this is fun'));
+    function rovarspraket(phrase) {
+      var newString = '';
+
+      phrase.split('').forEach(function(character){
+        if (!isVowel(character)){
+          newString += character + 'o' + character;
+        } else{
+          newString += character;
+        }
+      });
+      return newString.replace(' o', ' ');
+    }
+    console.log(rovarspraket('this is fun'));
 
 
 
@@ -84,9 +84,9 @@
 
 
 
-    //     function reverseString(str) {
-    //         return str.split('').reverse().join('');
-    //     }
-    //     reverseString('Haley');
-    //
-    // console.log(reverseString('haley'));
+        function reverseString(str) {
+            return str.split('').reverse().join('');
+        }
+        reverseString('Haley');
+
+    console.log(reverseString('haley'));
